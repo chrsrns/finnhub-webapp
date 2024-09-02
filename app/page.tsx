031fd22acd4111a2369047a28c0d1a9edb3aebae
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-09-02 21:01:46 +0800

    Renamed app

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 text-xl"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          ></a>
+          >
+            Finnhub Web App
+          </a>
         </div>
       </div>
 
