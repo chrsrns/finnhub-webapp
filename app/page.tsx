c0d90cd7db04d92d6e6d402218a98993920da045
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-09-02 23:20:55 +0800

    Modified app height to dynamic viewport

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex h-dvh flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed left-0 top-0 flex w-full items-end justify-center lg:static lg:size-auto">
           <a
