b114f545f8837cf6d1acbb167e96cd2104aba558
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-09-03 00:28:15 +0800

    Added placeholder event handlers and UI for stock search

--- app/finnhub-stocks.tsx
+++ app/finnhub-stocks.tsx
@@ -1,13 +1,40 @@
 "use client";
 
+import { ChangeEvent, FocusEvent, useState } from "react";
+import { Show } from "./utils";
+
 export default function FinnhubStocks() {
+  const [searchText, setSearchText] = useState("");
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+
+  // START handlers for the search text input element
+  function handleSearchTextChange(e: ChangeEvent<HTMLInputElement>) {
+    setSearchText(e.target.value);
+  }
+  function handleSearchInputFocus(_: FocusEvent<HTMLInputElement>) {
+    setIsSearchFocused(true);
+  }
+  function handleSearchInputBlur(_: FocusEvent<HTMLInputElement>) {
+    setIsSearchFocused(false);
+  }
+  // END
+
   return (
     <>
-      <div className="my-10 flex w-full flex-wrap place-items-center justify-center gap-4">
-        <input
-          placeholder="Enter stock symbol or name"
-          className="flex min-w-fit justify-center rounded-xl border border-b border-gray-300 bg-gray-200 bg-gradient-to-b from-zinc-200 p-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit"
-        ></input>
+      <div className="flex w-full flex-wrap place-items-center justify-center gap-4 py-10">
+        <div className="relative">
+          <input
+            value={searchText}
+            onChange={handleSearchTextChange}
+            onFocus={handleSearchInputFocus}
+            onBlur={handleSearchInputBlur}
+            placeholder="Enter stock symbol or name"
+            className="justify-center rounded-xl border border-b border-gray-300 bg-gray-200 bg-gradient-to-b from-zinc-200 p-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-900 dark:from-inherit"
+          ></input>
+          <Show when={isSearchFocused}>
+            <div className="top-100 absolute z-10 mt-2 h-32 w-full rounded border border-neutral-400 bg-zinc-900"></div>
+          </Show>
+        </div>
         <button
           className="focus:shadow-outline min-w-fit rounded bg-white px-4 py-2 font-bold text-black hover:bg-neutral-300 focus:outline-none"
           type="button"
