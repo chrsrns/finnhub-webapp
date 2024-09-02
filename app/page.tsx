fdf7dc05cde237c7cc35b7b10b4ffd14d69d2418
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-09-02 21:07:23 +0800

    Modified placement and size of app title

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+        <div className="fixed top-0 left-0 flex w-full items-end justify-center lg:static lg:size-auto">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 text-xl"
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 text-2xl"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
