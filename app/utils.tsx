4ced5eda098a7501cb65dd62d09925e4e444fdd0
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-09-03 23:27:24 +0800

    Added uuid utility
    This will be used to generate distinct keys for iterators

--- app/utils.tsx
+++ app/utils.tsx
@@ -8,4 +8,12 @@ const Show = ({ when, children }: { when: boolean; children: ReactNode }) => {
   return <></>;
 };
 
-export { Show };
+const uuidv4 = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
+
+export { Show, uuidv4 };
